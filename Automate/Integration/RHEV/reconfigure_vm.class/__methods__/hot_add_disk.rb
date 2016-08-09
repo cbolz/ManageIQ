dcbdee5f5b4467494a8c16ae318b68d2daa9e728
Christian Jung <chris@jung-christian.de>
2016-08-09 18:35:52 +0200

    added GPL disclaimer

--- Automate/Integration/RHEV/reconfigure_vm.class/__methods__/hot_add_disk.rb
+++ Automate/Integration/RHEV/reconfigure_vm.class/__methods__/hot_add_disk.rb
@@ -1,10 +1,20 @@
-###################################
 #
-# This method creates an additional disk and attaches it to the VM
+#            Automate Method
 #
-# install xmlsimple with "gem install xml-simple"
+# Copyright (C) 2016, Christian Jung
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-###################################
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 def attachhd(rhevmhost,rhevmuser,rhevmpass,vmuid,storage,size,allocpolicy)
   require 'rubygems'
