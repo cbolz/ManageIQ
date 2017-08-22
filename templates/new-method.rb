65a7f3e5677e7b8f16557f5afb455bca65f94fe0
Christian Jung <chris@bolzchristian.de>
2017-08-22 11:28:42 +0200

    updated template with license and less clutter

--- templates/new-method.rb
+++ templates/new-method.rb
@@ -1,25 +1,36 @@
 #
 #            Automate Method
 #
+# Copyright (C) 2016, Christian Jung
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 begin
-  @method = 'InspectMe'
-  $evm.log("info", "#{@method} - EVM Automate Method Started")
+  $evm.log("info", "EVM Automate Method Started")
 
   # Dump all of root's attributes to the log
-  $evm.root.attributes.sort.each { |k, v| $evm.log("info", "#{@method} Root:<$evm.root> Attribute - #{k}: #{v}")}
+  $evm.root.attributes.sort.each { |k, v| $evm.log("info", "Root:<$evm.root> Attribute - #{k}: #{v}")}
 
   #
   # Exit method
   #
-  $evm.log("info", "#{@method} - EVM Automate Method Ended")
+  $evm.log("info", "EVM Automate Method Ended")
   exit MIQ_OK
 
   #
   # Set Ruby rescue behavior
   #
 rescue => err
-  $evm.log("error", "#{@method} - [#{err}]\n#{err.backtrace.join("\n")}")
+  $evm.log("error", "[#{err}]\n#{err.backtrace.join("\n")}")
   exit MIQ_ABORT
 end
-
